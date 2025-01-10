cd1f7a88c2b916692bc23d597fa679d23578acff
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-10 22:03:00 +0530

    Add Math Opreations

---
+++ 07_Math/Random.java
@@ -0,0 +1,8 @@
+public class Random{
+    public static void main(String [] args){
+        int randomNum = (int)(Math.random() *101);
+
+        System.out.println((int)Math.random()*10);
+        System.out.println(randomNum);
+    }
+}
