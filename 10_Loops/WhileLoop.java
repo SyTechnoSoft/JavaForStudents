936999babd47d053027b540d5d5adaa450e29330
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-11 20:05:44 +0530

    Add Loop

---
+++ 10_Loops/WhileLoop.java
@@ -0,0 +1,23 @@
+
+public class WhileLoop {
+    public static void main(String[] argh)
+    {
+        int i=1;
+        // 1 to 10 table using while loop
+        while(i<=10)
+        {
+            System.out.print((i*2)+" ");
+        
+           i++;
+        }
+        int j=2;
+        while (j<=10) {
+            System.out.println(j+" ");
+            j+=2;
+            
+        }
+        
+
+        
+    }
+}
