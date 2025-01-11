413f4da2a86c38cce3537be2b78116b17fbe5860
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-11 10:21:55 +0530

    Add Booleans

---
+++ 08_Booleans/BooleanValues.java
@@ -0,0 +1,16 @@
+
+public class BooleanValues {
+    
+    public static void main(String[] args){
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        int x =5;
+        int y = 7;
+        System.out.println(x<y);          // Outputs true
+        System.out.println(x==4);          // Outputs false
+
+    }
+}
