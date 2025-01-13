1f852461f08d0214143453c4f31a8d4c19447657
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-13 23:11:35 +0530

    Table

--- 10_Loops/Table.java
+++ 10_Loops/Table.java
@@ -1,6 +1,7 @@
 public class Table {
     public static void main(String[]args){
 
+        // Create a table with 10 rows and 10 columns
         for(int i=1;i<=10;i++){
 
             for(int j=1;j<=10;j++){
