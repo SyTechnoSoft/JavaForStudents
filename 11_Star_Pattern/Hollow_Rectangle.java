7f43236ba71d564455805603572e2aaed8299d76
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-14 21:13:52 +0530

    Parallelogram

--- 11_Star_Pattern/Hollow_Rectangle.java
+++ 11_Star_Pattern/Hollow_Rectangle.java
@@ -1,6 +1,19 @@
 public class Hollow_Rectangle {
     public static void main(String[] args){
-        
+
+        int rows = 5;
+        int columns = 5;
+
+        for(int i = 1; i <= rows; i++){
+            for(int j = 1; j <= columns; j++){
+                if(i == 1 || i == rows || j == 1 || j == columns){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
     
 }
---
+++ 11_Star_Pattern/Parallelogram.java
@@ -0,0 +1,19 @@
+public class Parallelogram {
+    public static void main(String[] args){
+        for(int i=1; i<=5;i++){
+            for (int j=0; j<5; j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        
+                for(int k=0; k<i; k++){
+                    System.out.print(" ");
+           
+                 }
+         
+        }
+
+    }
+    
+}
