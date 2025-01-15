d5c9be5e197bb9035b4d1b6ca28220bc99355177
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-15 20:04:32 +0530

    :

--- 11_Star_Pattern/Mirrored_Rhombus.java
+++ 11_Star_Pattern/Mirrored_Rhombus.java
@@ -1,6 +1,18 @@
 public class Mirrored_Rhombus {
     
     public static void main(String[] args){
+
+        for(int i=4; i>=1; i--){
+            for(int j=1; j<=4; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+
+
+            
+            System.out.println("");
+        }
+       
        
     }
 }
