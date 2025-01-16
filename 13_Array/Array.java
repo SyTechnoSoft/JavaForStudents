9489aa7f874064d2c94a2785e16314e432d05972
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-16 23:07:03 +0530

    Array

--- 13_Array/Array.java
+++ 13_Array/Array.java
@@ -8,11 +8,13 @@ public class Array {
         //System.out.println(name); //[Ljava.lang.String;@4517d9a3
         System.out.println(Arrays.toString(name)); //[Saurabh Yadav, Abhishek Yadav, Gaurav Yadav, Aashu Yadav]
         System.out.println(name[0]); //Saurabh Yadav
+        System.out.println(name.length); //4
 
 
        // System.out.println(num); //[I@372f7a8d
         System.out.println(Arrays.toString(num)); //[10, 20, 30, 40, 50]
         System.out.println(num[4]); //50
+        System.out.println(num.length); //5
 
 
     }
