54643fb619f6c2749d9ccef8469aeec4429fdfc5
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-16 22:27:03 +0530

    For Each Loop

---
+++ 10_Loops/For_Each.java
@@ -0,0 +1,10 @@
+public class For_Each {
+    public static void main(String[] args){
+
+        String [] name = {"Saurabh", "Gaurav", "Abhishek", "Ankur"};
+        for (String i : name){
+            System.out.println(i);
+        }
+    }
+    
+}
