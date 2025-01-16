9e7779a81da8168a96c9ed80972601714842db3e
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-16 22:31:47 +0530

    For Each Int

---
+++ 10_Loops/For_Each_int.java
@@ -0,0 +1,10 @@
+public class For_Each_int {
+    public static void main(String[] args){
+
+        int [] num = {1, 2, 3, 4, 5};
+        for (int i : num){
+            System.out.println(i);
+        }
+    }
+    
+}
