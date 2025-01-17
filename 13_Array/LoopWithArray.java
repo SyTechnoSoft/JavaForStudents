54f100064169a384d961be2331ee9fe7693bfd56
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-17 21:37:28 +0530

    Array in Loop

---
+++ 13_Array/LoopWithArray.java
@@ -0,0 +1,9 @@
+public class LoopWithArray {
+    
+    public static void main(String[] args){
+        String[] name ={"Saurabh Yadav", "Abhishek Yadav", "Gaurav Yadav", "Aashu Yadav"};
+        for(int i=0; i<name.length; i++){
+            System.out.println(name[i]);
+        }
+    }
+}
