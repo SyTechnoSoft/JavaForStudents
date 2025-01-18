a033c5aaf514fd630ec9f9504f9302a4f912d405
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-18 10:13:07 +0530

    Multi Dimensional Array

---
+++ 13_Array/Multi_Dimensional_Array.java
@@ -0,0 +1,7 @@
+public class Multi_Dimensional_Array {
+    public static void main(String[] args){
+        int [][] myNumbers={{1,2,3,4,5}, {11,12,13,14,15}};
+        System.out.println(myNumbers[0][2]);
+    }
+    
+}
