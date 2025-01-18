08ecaaee6f80711e4277b3f844445c734c675fa7
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-18 10:35:05 +0530

    Method

--- 02_Java_Methods/01_Java_Method/Method.java
+++ 02_Java_Methods/01_Java_Method/Method.java
@@ -1,4 +1,10 @@
 
 public class Method {
-    
+    public static void main(String[] args) {
+        myMethod();
+    }
+
+    static void myMethod(){
+        System.out.println("My Method");
+    }
 }
