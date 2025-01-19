ee6919fe11e0ce319f18ea624a03464b601d0aea
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-19 21:17:17 +0530

    Method Parameter

---
+++ 02_Java_Methods/02_Method_Parameters/Parameter.java
@@ -0,0 +1,11 @@
+public class Parameter{
+    public static void main(String[] args){
+        method("Saurabh Yadav");
+        method("Gaurav Yadav");
+
+
+    }
+    static void method(String name){
+        System.out.println("Hello "+name);
+    }
+}
