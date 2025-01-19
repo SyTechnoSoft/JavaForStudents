2ff042dd8bf446ddb16fe9761aa8e8e939fab8c5
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-19 21:23:04 +0530

    Multiple Parameter

---
+++ 02_Java_Methods/02_Method_Parameters/Multiple_Parameter.java
@@ -0,0 +1,12 @@
+
+public class Multiple_Parameter {
+    public static void main(String[] args){
+        myMethod("Saurabh Yadav", 22);
+        myMethod("Gaurav Yadav", 18);
+    }
+
+    static void myMethod(String name, int age){
+        System.out.println("Hello "+name+" your age is "+age);
+    }
+    
+}
