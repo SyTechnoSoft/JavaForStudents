0225dd8606a2e26968905062ece940cdfb5748bd
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-20 21:50:00 +0530

    Return Int Value

--- 02_Java_Methods/03_Return/Return_Value.java
+++ 02_Java_Methods/03_Return/Return_Value.java
@@ -1,8 +1,8 @@
 public class Return_Value {
     public static void main(String[] args){
-        System.out.println(myMethod( 22));
+        System.out.println(myMethod( 17));
     }
     static int myMethod(int age){
-        return age;
+        return age + 5;
     }
 }
