267154ea9a1ff2b6971bd1885d813a7088faca6d
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-21 14:13:18 +0530

    Two Parameter

---
+++ 02_Java_Methods/03_Return/Two_Parameter.java
@@ -0,0 +1,10 @@
+public class Two_Parameter {
+    public static void main(String[] args) {
+        System.out.println(myMethod(5,4));
+    }
+    static int myMethod(int x, int y){
+        return x+y;
+
+    }
+    
+}
