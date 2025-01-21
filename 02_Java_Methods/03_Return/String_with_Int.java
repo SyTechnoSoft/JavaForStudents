efe9d980a8bcb2c1cc7be3be2c46998bdd7bd4f5
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-21 21:25:01 +0530

    String With Intger Value

--- 02_Java_Methods/03_Return/String_with_Int.java
+++ 02_Java_Methods/03_Return/String_with_Int.java
@@ -1,6 +1,8 @@
 public class String_with_Int{
     public static void main(String [] args){
-        System.out.println(myMethod("Saurabh Yadav ", 22));
+
+        String nameAge=myMethod("Saurabh Yadav ", 22);
+        System.out.println(nameAge);
     }
     static String myMethod(String name, int age){
         return name + age;
