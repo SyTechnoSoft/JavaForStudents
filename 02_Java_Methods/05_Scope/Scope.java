ca29d5fc745f40c5802f1d8f85b7c724464b6be3
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-01-23 20:03:30 +0530

    Scope

---
+++ 02_Java_Methods/05_Scope/Scope.java
@@ -0,0 +1,9 @@
+//Variables declared directly inside a method are available anywhere in the method following the line of code in which they were declared:
+public class Scope {
+
+    public static void main(String [] args){
+        int x=100;
+        System.out.println(x);
+    }
+    
+}
