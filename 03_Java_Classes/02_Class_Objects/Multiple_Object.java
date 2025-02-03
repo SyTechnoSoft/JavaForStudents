8d095a902e51a0930a745bce00fbce17242f4f05
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-03 22:54:37 +0530

    Multiple Object

---
+++ 03_Java_Classes/02_Class_Objects/Multiple_Object.java
@@ -0,0 +1,12 @@
+public class Multiple_Object {
+    int x=5;
+    public static void main(String[] args){
+
+        Multiple_Object obj1 = new Multiple_Object();  //Object 1
+        Multiple_Object obj2 = new Multiple_Object();  //Object 2
+        System.out.println(obj1.x);
+        System.out.println(obj2.x);
+
+    }
+    
+}
