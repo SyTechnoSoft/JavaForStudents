0b7f9af3610919d96be78e769321735b3aea50ca
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-03 22:48:01 +0530

    Object

---
+++ 03_Java_Classes/02_Class_Objects/Object.java
@@ -0,0 +1,10 @@
+public class Object {
+
+    int x = 5;
+    public static void main(String[] args) {
+
+        Object myObject =new Object();
+        System.out.println(myObject.x);
+    }
+    
+}
