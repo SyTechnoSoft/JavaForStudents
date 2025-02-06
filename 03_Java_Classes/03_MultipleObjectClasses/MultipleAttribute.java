4f6696279a76e6d782572f57f85e220ac6fb983e
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-06 21:30:47 +0530

    Multiple Attribute

---
+++ 03_Java_Classes/03_MultipleObjectClasses/MultipleAttribute.java
@@ -0,0 +1,13 @@
+public class MultipleAttribute {
+
+    String firstName="Saurabh";
+    String lastName="Yadav";
+    int age =22;
+
+    public static void main (String[] args){
+        MultipleAttribute myObj = new MultipleAttribute();
+        System.out.println("Name: "+myObj.firstName+" "+myObj.lastName);
+        System.out.println("Age: "+myObj.age);
+    }
+    
+}
