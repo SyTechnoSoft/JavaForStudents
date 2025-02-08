a5ef1bc9557c32124423adc9e7aab338afb0c1f1
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-08 21:53:20 +0530

    Static Vs. Public Method

---
+++ 03_Java_Classes/03_MultipleObjectClasses/StaticVsPublic.java
@@ -0,0 +1,21 @@
+public class StaticVsPublic {
+
+    //Static method
+    static void myStaticMethod(){
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    //Public method
+    public void myPublicMethod(){
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    //Main method
+    public static void main(String[] args){
+        myStaticMethod(); //Call the static method
+        //myPublicMethod(); //This would output an error
+        StaticVsPublic myObj = new StaticVsPublic(); //Create an object of StaticVsPublic
+        myObj.myPublicMethod(); //Call the public method
+    }
+    
+}
