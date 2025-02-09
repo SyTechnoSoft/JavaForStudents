911ce4f77533728dbad9715dfd303a3e5f74515f
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-09 17:08:40 +0530

    Methods With Objects

--- 03_Java_Classes/03_MultipleObjectClasses/MethodsWithObject.java
+++ 03_Java_Classes/03_MultipleObjectClasses/MethodsWithObject.java
@@ -10,5 +10,16 @@ public class MethodsWithObject {
         System.out.println("Max speed is: " +maxSpeed);
 
     }
-    
+    //Inside main, call the methods on the myCar object
+
+    //Main method
+    public static void main(String[] args){
+        MethodsWithObject methodsWithObject =new MethodsWithObject();
+        methodsWithObject.fullThrottle();
+        methodsWithObject.speed(100);
+    }
 }
+
+// Output
+// The car is going as fast as it can!
+// Max speed is: 100
