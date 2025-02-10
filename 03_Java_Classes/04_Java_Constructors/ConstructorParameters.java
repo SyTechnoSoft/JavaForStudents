64fcd6e07f194fafb3cccbe412d91de9ed857e7b
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-10 11:16:13 +0530

    Constructor Parameter

---
+++ 03_Java_Classes/04_Java_Constructors/ConstructorParameters.java
@@ -0,0 +1,12 @@
+// Constructors can also take parameters, which is used to initialize attributes.
+
+// The following example adds an int y parameter to the constructor.
+// Inside the constructor we set x to y (x=y). When we call the constructor,
+// we pass a parameter to the constructor (5), which will set the value of x to 5:
+
+public class ConstructorParameters {
+    int x;  // Create a class attribute
+
+    // Create a class constructor
+}
+
