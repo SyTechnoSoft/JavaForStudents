70bbfbda8b376ff741022eced3dbd266bb5e44b3
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-16 20:07:15 +0530

    Constructor many  Parameter

---
+++ 03_Java_Classes/04_Java_Constructors/ConstructorParameters2.java
@@ -0,0 +1,14 @@
+public class ConstructorParameters2 {
+    int modelYear;
+  String modelName;
+
+  public ConstructorParameters2(int year, String name) {
+    modelYear = year;
+    modelName = name;
+  }
+
+  public static void main(String[] args) {
+    ConstructorParameters2 myCar = new ConstructorParameters2(1969, "Mustang");
+    System.out.println(myCar.modelYear + " " + myCar.modelName);
+  }
+}
