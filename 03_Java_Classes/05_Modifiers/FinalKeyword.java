07a1dcb8da3deec838880ed3505f4793d8daba12
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-17 21:49:25 +0530

    Final Keyword

---
+++ 03_Java_Classes/05_Modifiers/FinalKeyword.java
@@ -0,0 +1,18 @@
+//If you don't want the ability to override existing attribute values, declare attributes as final:
+
+
+public class FinalKeyword {
+
+    final int x = 10; // x is a final attribute
+    final double  PI = 3.14; // PI is a final attribute
+
+    public static void main(String[] args){
+        FinalKeyword finalKeyword = new FinalKeyword();
+        finalKeyword.x = 50; // will generate an error: cannot assign a value to a final variable
+        finalKeyword.PI = 25; // will generate an error: cannot assign a value to a final variable
+        Sysytem.out.println(finalKeyword.x);
+
+    }
+    
+    
+}
