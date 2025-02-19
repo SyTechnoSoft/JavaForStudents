8f8f2e5206dd9e87b06e77566bcec8baeba8dca0
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-19 21:40:07 +0530

    Abstract

--- 03_Java_Classes/05_Modifiers/Abstract.java
+++ 03_Java_Classes/05_Modifiers/Abstract.java
@@ -20,7 +20,7 @@ class Student extends Main {
       // create an object of the Student class (which inherits attributes and methods from Main)
       Student myObj = new Student();
   
-      System.out.println("Name: " + myObj.fname);
+     // System.out.println("Name: " + myObj.fname);
       System.out.println("Age: " + myObj.age);
       System.out.println("Graduation Year: " + myObj.graduationYear);
       myObj.study(); // call abstract method
