eddcdb8c7ed552863eb2811235a63920f206ac4c
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-20 21:30:42 +0530

    Abstract

--- 03_Java_Classes/05_Modifiers/Abstract.java
+++ 03_Java_Classes/05_Modifiers/Abstract.java
@@ -1,4 +1,6 @@
-public class Abstract {
+
+
+abstract  class Abstract {
 
     public String fname = "John";
   public int age = 24;
@@ -6,7 +8,7 @@ public class Abstract {
     
 }
 // Subclass (inherit from Main)
-class Student extends Main {
+class Student extends Abstract {
     public int graduationYear = 2018;
     public void study() { // the body of the abstract method is provided here
       System.out.println("Studying all day long");
