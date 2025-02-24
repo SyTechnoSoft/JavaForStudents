baa8235f98f2f5a5ad6b3cf78fe19e098ae17a7f
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-02-24 22:18:48 +0530

    Package Scanner

--- 03_Java_Classes/07_Package/ScannerPackage.java
+++ 03_Java_Classes/07_Package/ScannerPackage.java
@@ -1,4 +1,5 @@
-/*Java Packages & API
+/*
+Java Packages & API
 A package in Java is used to group related classes. Think of it as a folder in a file directory. We use packages to avoid name conflicts, and to write a better maintainable code. Packages are divided into two categories:
 
 Built-in Packages (packages from the Java API)
