3a05a0ac61b04790974b3919a394ec830f2df8cd
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-03 21:42:24 +0530

    Formatting Date and Time

--- 03_Java_Classes/15_Date/FormattingDateTime.java
+++ 03_Java_Classes/15_Date/FormattingDateTime.java
@@ -9,9 +9,20 @@ public class FormattingDateTime {
 
         // Format the date-time value
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        DateTimeFormatter dateFormatter2 = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+        DateTimeFormatter timeFormatter2 = DateTimeFormatter.ofPattern("hh:mm:ss a");
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         
         String formattedDate = datetime.format(dateTimeFormatter);
         System.out.println("After formatting: " + formattedDate); // Output formatted date-time
+        System.err.println("dateFormatter: " + datetime.format(dateFormatter));
+        System.err.println("dateFormatter2: " + datetime.format(dateFormatter2));
+        System.err.println("dateTimeFormatter3: " + datetime.format(dateTimeFormatter3));
+        System.err.println("timeFormatter: " + datetime.format(timeFormatter));
+        System.err.println("timeFormatter2: " + datetime.format(timeFormatter2));
 
     }
     
