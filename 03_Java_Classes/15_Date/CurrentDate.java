1cfc55c0d078b2752ec88355da11a6e1fb32babb
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-03 20:54:52 +0530

    Current Date

---
+++ 03_Java_Classes/15_Date/CurrentDate.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+public class CurrentDate {
+
+    public static void main(String[] args){
+        LocalDate date=LocalDate.now(); //Create a date object
+        System.out.println(date); //Output current date
+    }
+
+    
+}
