b3fff4e50e851b9296b7cc1f51980e38331b703a
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-04 20:33:48 +0530

    ArrayList

---
+++ 04_Data_Structures/ArrayList1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class ArrayList1 {
+
+    public static void main(String[] args){
+        ArrayList<String> weekDays = new ArrayList<String>();
+        
+        weekDays.add("Monday");
+        weekDays.add("Tuesday");
+        weekDays.add("Wednesday");
+        weekDays.add("Thursday");
+        weekDays.add("Friday");
+        weekDays.add("Saturday");
+        weekDays.add("Sunday");
+        System.out.println(weekDays);
+    }
+    
+}
