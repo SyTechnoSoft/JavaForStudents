05eaf16c0829ef7418b83b1e546f3bda1ea8a91d
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-06 10:43:42 +0530

    ArrayList with Integer

---
+++ 04_Data_Structures/OtherType.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class OtherType{
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+    }
+}
