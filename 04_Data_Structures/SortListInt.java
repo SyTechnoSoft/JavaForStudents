8ed9e0bf1bfcb1076d30186bc047ced29eff6729
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-06 11:00:23 +0530

    Sort ArrayList Numerically

---
+++ 04_Data_Structures/SortListInt.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class SortListInt {
+
+    public static void main (String[] args){
+        ArrayList<Integer> numbers = new ArrayList <Integer>();
+        numbers.add(10);
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(4);
+        numbers.add(9);
+        Collections.sort(numbers);
+        for (int i : numbers){
+            System.out.println(i);
+        }
+    }
+    
+}
