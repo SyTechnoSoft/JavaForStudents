59cde159fe25b6847ba9e2098dce97ec28171fec
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-13 13:27:35 +0530

    toString() Method

---
+++ 04_Data_Structures/07_Wraper_Class/ToString.java
@@ -0,0 +1,9 @@
+public class ToString {
+
+    public static void main(String [] args){
+        Integer myInt = 1000;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+    }
+    
+}
