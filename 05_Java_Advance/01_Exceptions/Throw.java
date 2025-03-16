94e16ec49fe65e61ca3bbf810447aa8eebbf75f1
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-16 19:26:36 +0530

    Throw Keyword

---
+++ 05_Java_Advance/01_Exceptions/Throw.java
@@ -0,0 +1,17 @@
+public class Throw {
+    public static void main(String[] args){
+
+        //checkAge(15);// Set age to 15 (which is below 18...)
+        checkAge(20);// Set age to 20 (which is above 18...)
+
+    }
+
+    static void checkAge(int age){
+        if (age< 18){
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+    
+}
