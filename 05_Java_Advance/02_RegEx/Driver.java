66af39895fb37ff5b12fa56cf7461116a6c07378
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-20 22:00:02 +0530

    Greedy Quantifier

---
+++ 05_Java_Advance/02_RegEx/Driver.java
@@ -0,0 +1,23 @@
+
+    import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+ 
+// Driver Class
+class Driver
+{
+    public static void main(String[] args)
+    {
+        // Making an instance of Pattern class
+        // By default quantifier "+" is Greedy
+        Pattern p = Pattern.compile("g+");
+ 
+        // Making an instance of Matcher class
+        Matcher m = p.matcher("ggg");
+ 
+        while (m.find()){
+            System.out.println("Pattern found from " + m.start() +
+                               " to " + (m.end()-1));
+        }
+    }
+}
+
