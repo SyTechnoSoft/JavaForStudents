d2cfb821259155164d202b7fcbc3260f61db954b
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-03-23 22:33:45 +0530

    Regular Pattern

---
+++ 05_Java_Advance/02_RegEx/RegularPattern.java
@@ -0,0 +1,17 @@
+import java.util.regex.Pattern;
+public class RegularPattern {
+    
+    public static void main(String[] args) {
+        // Following line prints "true" because the whole
+        // text "geeksforgeeks" matches pattern
+        // "geeksforge*ks"
+        System.out.println(Pattern.matches(
+            "geeksforge*ks", "geeksforgeeks"));
+
+        // Following line prints "false" because the whole
+        // text "geeksfor" doesn't match pattern "g*geeks*"
+        System.out.println(Pattern.matches(
+              "g*geeks*", "geeksfor"));
+    }
+
+}
