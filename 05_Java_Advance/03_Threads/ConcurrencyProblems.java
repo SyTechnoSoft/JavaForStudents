6538d0f4e48151d0fa79042cf85e596866d17e86
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-04-01 22:50:30 +0530

    Use isAlive() to prevent concurrency problems

--- 05_Java_Advance/03_Threads/ConcurrencyProblems.java
+++ 05_Java_Advance/03_Threads/ConcurrencyProblems.java
@@ -1,3 +1,4 @@
+/*
 public class ConcurrencyProblems extends Thread {
     public static int amount = 0;
   
@@ -10,6 +11,33 @@ public class ConcurrencyProblems extends Thread {
     }
   
     @Override
+    public void run() {
+      amount++;
+    }
+  }*/
+
+  /* 
+   * To avoid concurrency problems, it is best to share as few attributes between threads as possible. 
+   * If attributes need to be shared, one possible solution is to use the isAlive() method of the thread to check whether the thread has finished running before using any attributes that the thread can change.
+   */
+
+   public class ConcurrencyProblems extends Thread {
+    public static int amount = 0;
+  
+    public static void main(String[] args) {
+        ConcurrencyProblems thread = new ConcurrencyProblems();
+      thread.start();
+      // Wait for the thread to finish
+      while(thread.isAlive()) {
+        System.out.println("Waiting...");
+      }
+      // Update amount and print its value
+      System.out.println("Main: " + amount);
+      amount++;
+      System.out.println("Main: " + amount);
+    }
+    @Override
+    // This method is executed when the thread starts
     public void run() {
       amount++;
     }
