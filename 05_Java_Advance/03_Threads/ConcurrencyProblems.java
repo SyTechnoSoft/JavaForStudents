11ab5f0707c1063e508d54f646422cc6e1dcd2c0
Saurabh Yadav <yadav.saurabh9517@gmail.com>
2025-04-01 22:47:07 +0530

    Concurrency Problems

---
+++ 05_Java_Advance/03_Threads/ConcurrencyProblems.java
@@ -0,0 +1,16 @@
+public class ConcurrencyProblems extends Thread {
+    public static int amount = 0;
+  
+    public static void main(String[] args) {
+      ConcurrencyProblems thread = new ConcurrencyProblems();
+      thread.start();
+      System.out.println(amount);
+      amount++;
+      System.out.println(amount);
+    }
+  
+    @Override
+    public void run() {
+      amount++;
+    }
+  }
